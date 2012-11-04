d5748a1616251e0d38435a9b62281aabbb0686a4
Tamás Gulácsi <gt-dev@gthomas.eu>
2012-11-04 09:33:06 +0100

    add DirLock

--- locking/locking.go
+++ locking/locking.go
@@ -3,6 +3,7 @@ package locking
 import (
 	"os"
 	"syscall"
+	"time"
 )
 
 type FLock struct {
@@ -38,4 +39,41 @@ func (lock FLock) TryLock() (bool, error) {
 func (lock FLock) Unlock() error {
 	lock.fh.Close()
 	return syscall.Flock(int(lock.fh.Fd()), syscall.LOCK_UN)
+}
+
+type DirLock string
+
+// create new directory-based lock (creates a subdir, if not exists, but unlocked first)
+func NewDirLock(path, name string) (DirLock, error) {
+	return DirLock(path + "/.lock-" + name), nil
+}
+
+func (lock DirLock) Lock() error {
+	var (
+		ok  bool
+		err error
+	)
+	for {
+		if ok, err = lock.TryLock(); ok && err == nil {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		time.Sleep(1)
+	}
+	panic("unreachable")
+}
+
+// acquire the lock, non-blocking
+func (lock DirLock) TryLock() (bool, error) {
+	err := os.Mkdir(string(lock), 0400)
+	if err == nil {
+		return true, nil
+	}
+	return false, nil
+}
+
+func (lock DirLock) Unlock() error {
+	return os.Remove(string(lock))
 }
