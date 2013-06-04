9a2fdc1b138c1e8205957102279fe29e0e54e328
Tamás Gulácsi <gt-waterhouse@gthomas.eu>
2013-06-04 05:32:38 +0200

    golint + LICENSE

--- locking.go
+++ locking.go
@@ -1,21 +1,29 @@
+// Copyright 2013 Tamás Gulácsi. All rights reserved.
+// Use of this source code is governed by an Apache 2.0
+// license that can be found in the LICENSE file.
+
+// Package locking contains file- and network (port) locking primitives
 package locking
 
 import (
 	"errors"
+	"net"
 	"os"
 	"path/filepath"
+	"strconv"
 	"syscall"
 	"time"
 )
 
+// AlreadyLocked is an error
 var AlreadyLocked = errors.New("AlreadyLocked")
 
+// FLock is a file-based lock
 type FLock struct {
 	fh *os.File
 }
-type FLocks []FLock
 
-//create new Flock-based lock (unlocked first)
+// NewFLock creates new Flock-based lock (unlocked first)
 func NewFLock(path string) (FLock, error) {
 	fh, err := os.Open(path)
 	if err != nil {
@@ -24,12 +32,12 @@ func NewFLock(path string) (FLock, error) {
 	return FLock{fh: fh}, nil
 }
 
-// acquire the lock, blocking
+// Lock acquires the lock, blocking
 func (lock FLock) Lock() error {
 	return syscall.Flock(int(lock.fh.Fd()), syscall.LOCK_EX)
 }
 
-// acquire the lock, non-blocking
+// TryLock acquires the lock, non-blocking
 func (lock FLock) TryLock() (bool, error) {
 	err := syscall.Flock(int(lock.fh.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
 	switch err {
@@ -41,11 +49,16 @@ func (lock FLock) TryLock() (bool, error) {
 	return false, err
 }
 
+// Unlock releases the lock
 func (lock FLock) Unlock() error {
 	lock.fh.Close()
 	return syscall.Flock(int(lock.fh.Fd()), syscall.LOCK_UN)
 }
 
+// FLocks is an array of FLocks, Unlockable at once
+type FLocks []FLock
+
+// FLockDirs returns FLocks for each directory
 func FLockDirs(dirs ...string) (FLocks, error) {
 	locks := make([]FLock, 0, len(dirs))
 	allright := false
@@ -76,15 +89,18 @@ func FLockDirs(dirs ...string) (FLocks, error) {
 	return FLocks(locks), nil
 }
 
+// Unlock all locks
 func (locks FLocks) Unlock() {
 	for _, lock := range locks {
 		lock.Unlock()
 	}
 }
 
+// DirLock is a directory lock
 type DirLock string
 
-// create new directory-based lock (creates a subdir, if not exists, but unlocked first)
+// NewDirLock create new directory-based lock
+// (creates a subdir, if not exists, but unlocked first)
 // WARNING: no automatic Unlock on exit/panic!
 func NewDirLock(path string) (DirLock, error) {
 	fi, err := os.Lstat(path)
@@ -99,7 +115,7 @@ func NewDirLock(path string) (DirLock, error) {
 	return DirLock(path), nil
 }
 
-// locks (creates .lock subdir)
+// Lock locks (creates .lock subdir)
 func (lock DirLock) Lock() error {
 	var (
 		ok  bool
@@ -114,10 +130,9 @@ func (lock DirLock) Lock() error {
 		}
 		time.Sleep(1)
 	}
-	panic("unreachable")
 }
 
-// acquire the lock, non-blocking
+// TryLock acquires the lock, non-blocking
 func (lock DirLock) TryLock() (bool, error) {
 	err := os.Mkdir(string(lock), 0600)
 	if err == nil {
@@ -126,6 +141,39 @@ func (lock DirLock) TryLock() (bool, error) {
 	return false, nil
 }
 
+// Unlock releases the directory lock
 func (lock DirLock) Unlock() error {
 	return os.Remove(string(lock))
+}
+
+// PortLock is a locker which locks by binding to a port on the loopback IPv4 interface
+type PortLock struct {
+	hostport string
+	ln       net.Listener
+}
+
+// NewPortLock returns a lock for port
+func NewPortLock(port int) *PortLock {
+	return &PortLock{hostport: net.JoinHostPort("127.0.0.1", strconv.Itoa(port))}
+}
+
+// Lock locks on port
+func (p *PortLock) Lock() {
+	var err error
+	t := 1 * time.Second
+	for {
+		if p.ln, err = net.Listen("tcp", p.hostport); err == nil {
+			return
+		}
+		//log.Printf("spinning lock on %s (%s)", p.hostport, err)
+		time.Sleep(t)
+		t = time.Duration(float32(t) * 1.2)
+	}
+}
+
+// Unlock unlocks the port lock
+func (p *PortLock) Unlock() {
+	if p.ln != nil {
+		p.ln.Close()
+	}
 }
